5662e0e408b4612201632cc855eb6a5a2b1fab82
ducnguyen96 <levinguyen.dl@gmail.com>
2024-12-25 14:32:39 +0700

    chore: add a little bit of wait time when word is not scraped

--- src/lib/server/queries/dictionary/read.ts
+++ src/lib/server/queries/dictionary/read.ts
@@ -3,6 +3,9 @@ import { scrapeLangeek } from "../../services/scraper";
 
 export const read = async (dto: FindDTO) => {
   const found = await findFirst(dto);
-  if (!found) await scrapeLangeek(dto);
+  if (!found) {
+    await scrapeLangeek(dto);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+  }
   return findFirst(dto);
 };

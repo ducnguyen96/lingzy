5bae9d0df27afcb2015dedda5fc2284707a0ad3f
ducnguyen96 <levinguyen.dl@gmail.com>
2024-12-25 14:31:16 +0700

    chore: smooth scroll

--- src/components/dictionary/translation-group-box.tsx
+++ src/components/dictionary/translation-group-box.tsx
@@ -14,14 +14,11 @@ export default function TranslationGroupBox({
   type,
 }: TranslationGroupBoxProps) {
   return (
-    <div className={className}>
+    <div className={className} id={type}>
       <div className="border rounded-2xl">
         <div className="flex items-center justify-between bg-blue-50 p-4 rounded">
           <span className="text-2xl">{title}</span>
-          <span
-            id={type}
-            className="bg-teal-500 text-white text-xs font-bold px-2 py-1 rounded uppercase"
-          >
+          <span className="bg-teal-500 text-white text-xs font-bold px-2 py-1 rounded uppercase">
             {type}
           </span>
         </div>

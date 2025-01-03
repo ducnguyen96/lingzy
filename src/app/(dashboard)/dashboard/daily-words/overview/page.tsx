61d267342bb46e38735a7bad7ded3fa9d36a97f7
ducnguyen96 <levinguyen.dl@gmail.com>
2025-01-03 18:37:19 +0700

    chore: break down overview component

--- src/app/(dashboard)/dashboard/daily-words/overview/page.tsx
+++ src/app/(dashboard)/dashboard/daily-words/overview/page.tsx
@@ -1,11 +1,43 @@
+import EveryComponent from "@/components/dashboard/daily-words/every-component";
 import Overview from "@/components/dashboard/daily-words/overview";
+import { everyComp } from "@/config/overview";
 import { getDailyWordsOverview } from "@/lib/server/queries/user/daily-words";
+import { GraduationCap, Hourglass } from "lucide-react";
 
 export default async function Page() {
   const overview = await getDailyWordsOverview();
+  const listUrl = "/dashboard/daily-words/list";
+
   return (
-    <>
+    <div className="h-full justify-center items-center lg:p-4 space-y-4">
       <Overview overview={overview} />
-    </>
+      <div className="p-6 gap-6 bg-primary/5 grid grid-cols-1 lg:grid-cols-2 rounded-3xl">
+        <EveryComponent
+          href={`${listUrl}/everyday`}
+          badge={<Hourglass size={14} />}
+          color="orange-400"
+          text="Waiting"
+          value={overview.waiting}
+        />
+        {Object.entries(everyComp).map(([key, { title }], idx) => (
+          <EveryComponent
+            key={key}
+            href={`${listUrl}/${key}`}
+            badge={<span>{idx + 1}</span>}
+            color="purple-400"
+            text={title}
+            // @ts-expect-error valid key
+            value={overview[key]}
+          />
+        ))}
+        <EveryComponent
+          href={`${listUrl}/learned`}
+          badge={<GraduationCap size={14} />}
+          color="green-400"
+          text="Learned"
+          value={overview.learned}
+        />
+      </div>
+    </div>
   );
 }
---
+++ src/components/dashboard/daily-words/every-component.tsx
@@ -0,0 +1,51 @@
+import { cn } from "@/lib/utils";
+import { ArrowRight, CalendarSync } from "lucide-react";
+import Link from "next/link";
+import { cloneElement, JSX } from "react";
+
+interface EveryComponentProps {
+  href: string;
+  text: string;
+  value: number;
+  badge: JSX.Element;
+  color: string;
+}
+
+export default function EveryComponent(props: EveryComponentProps) {
+  const { text, value, badge, color, href } = props;
+
+  return (
+    <Link
+      prefetch={false}
+      className="flex items-center gap-4 group"
+      href={href}
+    >
+      <div className="relative">
+        <CalendarSync size={40} className={`text-${color}`} strokeWidth={1} />
+        <div
+          className={cn(
+            "absolute flex items-center justify-center w-5 h-5 text-sm font-semibold rounded-full -top-1 -right-2",
+            `bg-${color}`,
+          )}
+        >
+          {cloneElement(badge)}
+        </div>
+      </div>
+      <div className="flex flex-1 bg-background rounded-3xl p-4 items-center justify-between">
+        <div className="flex items-center gap-2 lg:gap-4">
+          <span className="lg:text-lg">{text}</span>
+          <div
+            className={cn(
+              "p-2 py-1 space-x-2 bg-opacity-50 rounded-3xl",
+              `text-${color} bg-${color}`,
+            )}
+          >
+            <span className="font-semibold textsm">{value}</span>
+            <span>Word</span>
+          </div>
+        </div>
+        <ArrowRight />
+      </div>
+    </Link>
+  );
+}
